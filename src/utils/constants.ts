0efe8d584186eba98369e84b043f35d32b99f2c4
Avner Israel <avner@safebase.io>
2023-04-28 13:47:19 +0300

    chore: open the platform to all

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,5 @@ export const MAX_UPLOAD_FILE_SIZE = 10 * 1024 * 1024;
 export const IMAGES_MIME_TYPE = 'image/*';
 
 export const MAX_FILE_UPLOAD_COUNT = 5;
+
+export const IMAGE_RESIZE_QUEUE = 'image-resize-queue';
